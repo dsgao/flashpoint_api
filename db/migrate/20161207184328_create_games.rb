a888d50e5ef4df0e903cb1101b45a5a977cfb9f6
David <dsgao@andrew.cmu.edu>
2016-12-07 14:31:20 -0500

    Removed emails column.

--- db/migrate/20161207184328_create_games.rb
+++ db/migrate/20161207184328_create_games.rb
@@ -1,8 +1,8 @@
 class CreateGames < ActiveRecord::Migration[5.0]
   def change
     create_table :games do |t|
-      t.int :asker_id
-      t.int :teller_id
+      t.integer :asker_id
+      t.integer :teller_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207050442) do
+ActiveRecord::Schema.define(version: 20161207192808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,10 +31,17 @@ ActiveRecord::Schema.define(version: 20161207050442) do
     t.integer  "shared_ids",              array: true
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "games", force: :cascade do |t|
+    t.integer  "asker_id"
+    t.integer  "teller_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "email"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "facebook_id"
   end
 
 end
