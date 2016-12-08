6b2a457540117b0593433921b1f0a070fea12684
David <dsgao@andrew.cmu.edu>
2016-12-07 20:12:48 -0500

    Added a new route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 	resources :cards, except: [:new, :edit]
 	resources :decks, except: [:new, :edit]
 	resources :users, except: [:new, :edit]
+	resources :games, except: [:new, :edit]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
